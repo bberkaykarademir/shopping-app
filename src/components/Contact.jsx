2767e9d1645449b56205230ca51e04ce90c88ce4
bberkaykarademir <bberkaykarademir@hotmail.com>
2023-04-06 14:18:46 +0300

    email.js

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Navbar from "./Navbar";
+import emailjs from "@emailjs/browser";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Contact = ({ activeLink, setActiveLink, handleClick }) => {
   useEffect(() => {
     setActiveLink(3);
   }, []);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_4jw1hxx",
+        "template_hx712nr",
+        e.target,
+        "-jDpJH8ZJs4oaPchN"
+      )
+      .then(() => {
+        toast("sended!");
+      });
+    e.target.reset();
+  };
   return (
     <>
       <Navbar
@@ -16,18 +35,18 @@ const Contact = ({ activeLink, setActiveLink, handleClick }) => {
         <img src="contactus.jpg" />
         <div className="contact-text">
           <h1>Contact Us</h1>
-          <form>
+          <form onSubmit={sendEmail}>
             <div>
               <p>Full Name:</p>
-              <input type="text" name="fullname" />
+              <input type="text" name="fullname" required />
             </div>
             <div>
               <p>Email Address:</p>
-              <input type="email" name="email" />
+              <input type="email" name="email" required />
             </div>
             <div>
               <p>Message:</p>
-              <textarea name="message" rows="5"></textarea>
+              <textarea name="message" rows="5" required></textarea>
             </div>
             <div>
               <button type="submit">Send Message</button>
@@ -35,6 +54,7 @@ const Contact = ({ activeLink, setActiveLink, handleClick }) => {
           </form>
         </div>
       </div>
+      <ToastContainer />
     </>
   );
 };
